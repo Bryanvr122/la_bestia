316185396654dea4a1c823a33dbc38455d9cd900
Bryanvr122 <72177879+Bryanvr122@users.noreply.github.com>
2026-09-09 23:39:19 -0400

    Update la_bestia.py

--- la_bestia.py
+++ la_bestia.py
@@ -79,5 +79,5 @@ def webhook_receiver():
 def index_check():
         return "=== [NEXUS ALFA V7.0] TOYOTA AVALON INMORTAL ACTIVO EN RENDER ==="
 
-if __name__ == "__main__":
+    if __name__ == "__main__":
     app.run(port=5000)
